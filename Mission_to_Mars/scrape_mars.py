5013fce2c60582b969cc5af41d4e3e64d58c480b
RomaPatel7 <50124854+RomaPatel7@users.noreply.github.com>
2019-11-13 18:27:35 -0500

    Update scrape_mars.py

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
